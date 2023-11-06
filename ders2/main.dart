f47127d4d5f92c3a7be7af9927e58b0084f2dd13
GelecekVadisi <gelecekvadisiorg@gmail.com>
2023-11-06 15:49:20 +0300

    2. Ders kodları eklendi

---
+++ ders2/main.dart
@@ -0,0 +1,41 @@
+void main() {
+  //  Aşağıdaki kod bir selamlama metnini ekrana yazdırır.
+
+  /*  
+  Aşağıdaki kod
+  bir selamalama metnini
+  ekrana yazdırır.
+  */
+  /* print("Merhaba Ahmet!");
+  print('Merhaba Ahmet! (tek tırnak kullanıldı)');
+  print("Ahmet, \"Bugün hava çok güzel\" dedi");
+  print('Ahmet, \'Bugün hava çok güzel\' dedi');
+  print("Ahmet, 'Bugün hava çok güzel' dedi");
+  print('Ahmet, "Bugün hava çok güzel" dedi'); */
+
+  // degisken_tipi degisken_adi = veri;
+  String name = "Ahmet";
+  int sayi = 5;
+  int kredi = 3000;
+  double bakiye = 130.45;
+  num sayi2 = 123;
+  var degisken = 123;
+  bool krediAliyorMu = false;
+
+  //  Cinsiyet değeri true ise Erkek manasına gelir.
+  bool cinsiyet = true;
+  bool erkekMi = true;
+  bool mezunMu = true;
+  bool havaGunesliMi = false;
+
+  print("Merhaba " + name + "!");
+  print("Merhaba $name! Bugün hava çok güzel.");
+  print("Merhaba $name! Kredi limitiniz $kredi\$ dir.");
+  print("Merhaba $name!");
+  print("Merhaba $name!");
+  print("Merhaba $name!");
+  print("Merhaba $name!");
+  print("Bakiye: " + bakiye.toString());
+  print(sayi + sayi);
+  print('bglsbgklegbs `lbflaksfblsak` sbajlfbsal');
+}
---
+++ ders2/obs.dart
@@ -0,0 +1,20 @@
+void main() {
+  String name = "Ahmet";
+  String lastName = "Çakır";
+  String city = "İstanbul";
+  String ogrNo = "0214681206408";
+  String tc = "12345678901";
+  int age = 19;
+  int sinif = 2;
+  int dersSayisi = 6;
+  bool erkekMi = true;
+  bool mezunMu = false;
+  double gno = 3.56;
+  String danisman = "Ali";
+  double sinavNotu = 87.5;
+  String cinsiyet = "e";
+  String kulubu = "Bilgisayar Topluluğu";
+
+  print(
+      "Merhaba $name $lastName. Yaşınız: $age, Cinsiyet: $cinsiyet, GNO: $gno, Kulübü: $kulubu");
+}
